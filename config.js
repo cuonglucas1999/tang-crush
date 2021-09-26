14562863752eceeb87520a9ffa393fc72edccc55
cuonglucas1999 <91431359+cuonglucas1999@users.noreply.github.com>
2021-09-27 02:09:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Nguyễn Thị Ngọc Hà,Babi à!',
+    introDesc: `Cảm ơn em 2 tháng qua đã cùng anh cố gắng
+    Anh hơi bị đần vì không biết chuẩn bị cho em như nào
+    Lần đầu tiên anh làm cái này 
+    Vì bé rất đặc biệt với anh <3`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không, không và không lucass à :3',
+    question:'Điều anh băn khoăn hôm nay đó là liệu rằng bé có thể cho anh 1 cơ hội làm người yêu thương em nhất trên đời không? <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết bé sẽ đồng ý mà 🥰. Yêu em bé nhiều nhiều 😘😘',
+    messDesc: 'Tối mai em bé bận thì ngày kia anh qua đón em đi chơi nhá.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
