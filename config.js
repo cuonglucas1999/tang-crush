82b75c0405f8cebdf28796c2fb2369630bda29f8
cuonglucas1999 <91431359+cuonglucas1999@users.noreply.github.com>
2021-09-27 01:28:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Nguyễn Thị Ngọc Hà !',
-    introDesc: `Anh biết chúng ta trong 2 tháng qua đã cố gắng rất nhiều
-    Anh đã hơi bị đần vì không biết phải chuẩn bị cho bé như nào
-    Đây là lần đầu anh làm cái này vì bé rất đặc biệt với anh
-    Đằng sau chữ yêu đây là thương bé rất nhiều`,
+    introDesc: `Anh biết chúng ta trong 2 tháng qua đã cố gắng rất nhiều.
+    Anh đã hơi bị đần vì không biết phải chuẩn bị cho bé như nào.
+    Đây là lần đầu anh làm cái này vì bé rất đặc biệt với anh.
+    Đằng sau chữ yêu đây là thương bé rất nhiều.`,
     btnIntro: 'hihi',
     title: 'Phải chăng em bé đang rất nhớ anh nhỉ 😙',
     desc: 'Nhận mạnh là đừng cố bấm vào nút Không đấy ^^',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối mai 8h anh qua gặp em nhé tình yêu của anh.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+   
 }
